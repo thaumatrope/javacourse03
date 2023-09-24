3702521dea5c9d1b69dae18bbd01568389e8d0ea
John Westfield <j.westfield@gmx.de>
2023-09-24 20:27:05 +0200

    initial commit

---
+++ ThingsWithStrings/src/application/App.java
@@ -0,0 +1,19 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "John";
+		
+		System.out.println("My name is: " + name);
+
+		String endOfSentence = ".";
+		String text = "I am called ";
+		System.out.println(text + name + endOfSentence);
+		
+		
+	}
+
+}
