79d255ac3f44d255aa873b828abd85c1d9e2c363
John Westfield <j.westfield@gmx.de>
2023-09-24 20:43:27 +0200

    initial commit

---
+++ PublicAndPrivate/src/application/Person.java
@@ -0,0 +1,23 @@
+package application;
+
+public class Person {
+	private String name;
+	private int age;
+	
+	public void setName(String text) {
+		this.name = text;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setAge(int number) {
+		this.age = number;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+
+}
