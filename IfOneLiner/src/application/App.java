70a54fbc7ad497579d3cb2a3b384e697e350c88a
John Westfield <j.westfield@gmx.de>
2023-09-24 20:52:23 +0200

    initial commit

---
+++ IfOneLiner/src/application/App.java
@@ -0,0 +1,17 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Boolean isRaining = false;
+		
+		if(isRaining)
+			System.out.println("It's raining!");
+		else
+			System.out.println("it's not raining!");
+
+	}
+
+}
