cb1dcd525e36cb706d8fed83d5efd6a3c120c250
John Westfield <j.westfield@gmx.de>
2023-09-24 20:51:07 +0200

    initial commit

---
+++ MainMethod/src/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");		
+		System.out.println("This is another line of text!");
+		System.out.println("Final line of text");
+
+	}
+
+}
