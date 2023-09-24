d072ff5df42bb8438a56797dd89addc079776417
John Westfield <j.westfield@gmx.de>
2023-09-24 20:39:20 +0200

    initial commit

---
+++ PrimitiveArraysCloserLook/src/application/App.java
@@ -0,0 +1,28 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int value;
+		
+		int[] values;
+		
+		values = null;
+		
+		value = 123;
+		
+		System.out.println(value);
+		
+		System.out.println(values);
+		
+		values = new int[3];
+		
+		for(int n: values) {
+			System.out.println(n);
+		}
+
+	}
+
+}
