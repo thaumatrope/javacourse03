0505d554180ecf1bfb5513b0d4662a0dcac98abc
John Westfield <j.westfield@gmx.de>
2023-09-24 14:18:36 +0200

    initial commit

---
+++ Boolean/src/application/App.java
@@ -0,0 +1,30 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean condition1 = true;
+
+		System.out.println("Condition1 is:" + condition1);
+
+		boolean condition2 = false;
+
+		System.out.println("Condition2 is:" + condition2);
+
+		boolean condition3 = 4 > 5;
+
+		System.out.println("Condition2 is:" + condition3);
+
+		boolean condition4 = 12 > 6;
+
+		System.out.println("Condition2 is:" + condition4);
+
+		//boolean condition5 = null;
+
+		//System.out.println("Condition2 is:" + condition5);
+
+	}
+
+}
