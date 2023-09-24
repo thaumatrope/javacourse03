9dbe09cb0be46d45dad8477e45d326d2dfb67f51
John Westfield <j.westfield@gmx.de>
2023-09-24 20:32:23 +0200

    initial commit

---
+++ Enum/src/application/App.java
@@ -0,0 +1,17 @@
+package application;
+
+//import java.awt.Color;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Color color;
+		
+		Fruit fruit1 = Fruit.APPLE;
+		Fruit fruit2 = Fruit.ORANGE;
+		
+		System.out.println(fruit1 == fruit2);
+	}
+
+}
---
+++ Enum/src/application/Fruit.java
@@ -0,0 +1,6 @@
+package application;
+
+public enum Fruit {
+
+	APPLE, BANANA, ORANGE
+}
