658597a28d8c8844f2a03ee3f1f9255b0870db8e
John Westfield <j.westfield@gmx.de>
2023-09-24 20:22:05 +0200

    initial commit

---
+++ VariablesDouble/src/application/Double.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Double {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double height = 1.6;
+		
+		System.out.println("Hello World!");
+		System.out.println("Height is: " + height);
+	}
+
+}
