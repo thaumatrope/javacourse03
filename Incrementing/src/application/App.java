77ae57f169b01fb4cc8a68e1b312f72df3876243
John Westfield <j.westfield@gmx.de>
2023-09-24 20:52:11 +0200

    initial commit

---
+++ Incrementing/src/application/App.java
@@ -0,0 +1,37 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int count = 0;
+		System.out.println(count);
+		
+		count = count + 1;		
+		System.out.println(count);
+		
+		count += 1;		
+		System.out.println(count);
+		
+		count++; //Postfix		
+		System.out.println(count);
+		
+		++count; //Prefix		
+		System.out.println(count);
+		
+		//decrement		
+		count -= 1;		
+		System.out.println(count);
+		
+		count--; //Postfix		
+		System.out.println(count);
+		
+		--count; //Prefix		
+		System.out.println(count);
+		
+	
+
+	}
+
+}
