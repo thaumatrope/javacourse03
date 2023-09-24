4602f45b8f6b3158fe150a2e1cc80c179f91a4dc
John Westfield <j.westfield@gmx.de>
2023-09-24 14:58:07 +0200

    initial commit

---
+++ Arrays/src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] numbers = {2,4,6,8};
+		
+		/*
+		 * Shopping list
+		 * 1. Tomatoes
+		 * 2. Pasta
+		 * 3. Onions
+		 * 
+		 * Array-Indexes are zero-based
+		 */
+		
+		System.out.println(numbers[0]); // first element
+		System.out.println(numbers[2]);
+		System.out.println(numbers[3]);	// last element
+		System.out.println(numbers[6]); // out of index
+
+	}
+
+}
