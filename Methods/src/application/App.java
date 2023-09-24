0d1a32ab2725e833bc00adf14527a425021c9edb
John Westfield <j.westfield@gmx.de>
2023-09-24 20:49:09 +0200

    initial commit

---
+++ Methods/src/application/App.java
@@ -0,0 +1,17 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person person1 = new Person();
+		person1.name = "Joe";
+		
+		System.out.println(person1.name);
+		
+		person1.sayHello();
+
+	}
+
+}
