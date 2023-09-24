46ca863212c84fc21c29909035cb1093ca7d85af
John Westfield <j.westfield@gmx.de>
2023-09-24 20:39:06 +0200

    initial commit

---
+++ StreamLoops/src/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.stream.IntStream;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		IntStream.range(0, 5).forEach(i -> {
+			System.out.println(i);
+		});
+
+	}
+
+}
