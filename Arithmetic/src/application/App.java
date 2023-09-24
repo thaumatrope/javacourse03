f9ff5595c395920524ac71a4aa300f2781591358
John Westfield <j.westfield@gmx.de>
2023-09-24 14:54:40 +0200

    initial commit

---
+++ Arithmetic/src/application/App.java
@@ -0,0 +1,25 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Plus +
+		// Divide /
+		// Multiply *
+		// Subtract -
+		
+		System.out.println(7 + 3);
+		System.out.println(9 / 3);
+		
+		//Surprise
+		System.out.println(10 / 3);
+		System.out.println(10.0 / 3.0);
+		System.out.println(9 / 4.0);  // one floating point
+		
+		System.out.println(9 * 3);
+		System.out.println(3 - 4);
+
+	}
+
+}
