abd5f98191d8515abe67e3097d4be09533f055a7
John Westfield <j.westfield@gmx.de>
2023-09-24 20:42:51 +0200

    initial commit

---
+++ StaticInnerClasses/src/application/App.java
@@ -0,0 +1,15 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person p1 = new Person();
+		p1.print();
+		
+		Person.Head head = new Person.Head();
+		head.print();
+	}
+
+}
