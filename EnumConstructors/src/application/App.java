6cd81ea2550ebac278725d196c426d60c92173c8
John Westfield <j.westfield@gmx.de>
2023-09-24 20:32:43 +0200

    initial commit

---
+++ EnumConstructors/src/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+//import java.awt.Color;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Color color;
+		
+		System.out.println(Fruit.APPLE);
+		System.out.println(Fruit.BANANA);
+		System.out.println(Fruit.ORANGE);
+	}
+
+}
---
+++ EnumConstructors/src/application/Fruit.java
@@ -0,0 +1,16 @@
+package application;
+
+public enum Fruit {
+
+	APPLE("green"), BANANA("yellow"), ORANGE("orange");
+	
+	private String color;
+	
+	Fruit(String color) {
+		this.color = color;
+	}
+	
+	public String toString() {
+		return super.toString().toLowerCase() + " (" + color +")";
+	}
+}
