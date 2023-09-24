3920e11ff970fc0b19bbdf1c15299c19ac9a4d93
John Westfield <j.westfield@gmx.de>
2023-09-24 20:48:57 +0200

    initial commit

---
+++ MoreWhileLoops/src/application/App.java
@@ -0,0 +1,19 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int count = 0;
+		
+		var count = 0; //typeless declaration var-Type
+		
+		
+		while (count < 10) {
+			System.out.printf("Count is: %d\n", count++);
+			//count++;
+		}
+	}
+
+}
