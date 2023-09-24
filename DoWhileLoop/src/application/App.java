fc1228cb4656a9ca0d5fc585dfde1f8ffa27888a
John Westfield <j.westfield@gmx.de>
2023-09-24 20:30:28 +0200

    initial commit

---
+++ DoWhileLoop/src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int count = 0;
+		
+		System.out.println("First Loop:");
+		do {
+			System.out.println("Count: " + count);
+			
+		}while(count++ < 5);
+				
+		count = 0;
+		
+		System.out.println("Second Loop:");
+		do {
+			System.out.println("Count: " + count);
+			
+		}while(count++ < 3);
+		
+	}
+
+}
