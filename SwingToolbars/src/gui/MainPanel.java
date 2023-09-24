b665357744dd9a5524937c2363c30c15034b233e
John Westfield <j.westfield@gmx.de>
2023-09-24 20:31:52 +0200

    initial commit

---
+++ SwingToolbars/src/gui/MainPanel.java
@@ -0,0 +1,13 @@
+package gui;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel {
+	
+	public MainPanel() {
+		this.setBackground(Color.green);
+	}
+
+}
