d082903203375db16a4cf979061bb0a116bce349
John Westfield <j.westfield@gmx.de>
2023-09-24 20:24:22 +0200

    initial commit

---
+++ UserPicksObject/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+import game.Game;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+		new Game().run();
+	}
+
+}
---
+++ UserPicksObject/src/game/objects/Rock.java
@@ -0,0 +1,10 @@
+package game.objects;
+
+public class Rock extends GameObject{
+	
+	public Rock() {
+		setId(0); 
+		setName("Rock");
+	}
+
+}
