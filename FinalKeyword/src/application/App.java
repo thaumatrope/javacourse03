2ae4bd0ab872dda4a9e1305fe4e15f13fe030367
John Westfield <j.westfield@gmx.de>
2023-09-24 20:34:26 +0200

    initial commit

---
+++ FinalKeyword/src/application/App.java
@@ -0,0 +1,27 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//final int value = 9;
+		final int VALUE = 10; // UPPERCASE for const
+		
+		System.out.println(VALUE);
+		
+		//VALUE = 11;
+		
+		//System.out.println(VALUE);
+		
+		//String userPassword = "abc";
+		final String USERPASSWORD = "abc";
+		
+		//USERPASSWORD = "egc";
+		System.out.println(USERPASSWORD);
+		
+		
+
+	}
+
+}
