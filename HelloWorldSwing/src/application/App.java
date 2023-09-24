b4a0500b1e46441e231274bcb799f798c0976c03
John Westfield <j.westfield@gmx.de>
2023-09-24 20:46:58 +0200

    initial commit

---
+++ HelloWorldSwing/src/application/App.java
@@ -0,0 +1,20 @@
+package application;
+
+import javax.swing.JFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame mainFrame = 	new JFrame(); // represents a window
+		
+		mainFrame.setSize(600, 400);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mainFrame.setVisible(true);
+		
+		
+		
+	}
+
+}
