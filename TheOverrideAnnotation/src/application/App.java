dab04b6756294b942a00bca2913819c8888b3c78
John Westfield <j.westfield@gmx.de>
2023-09-24 20:27:21 +0200

    initial commit

---
+++ TheOverrideAnnotation/src/application/App.java
@@ -0,0 +1,21 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HouseCat cat1 = new HouseCat();
+		cat1.vocalize();
+		cat1.hunt();
+		
+		Tiger cat2 = new Tiger();
+		cat2.vocalize();
+		cat2.hunt();
+		
+		Lion cat3 = new Lion();
+		cat3.vocalize();
+		cat3.hunt();
+	}
+
+}
