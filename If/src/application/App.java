035aabed2d696b8275d0953a6275cce8c5a58244
John Westfield <j.westfield@gmx.de>
2023-09-24 20:38:17 +0200

    initial commit

---
+++ If/src/application/App.java
@@ -0,0 +1,18 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int apples = 4;
+		int bananas = 5;
+		
+		if(apples < bananas) {
+			System.out.println("More apples than bananas.");
+		}
+		
+		System.out.println("End of program.");
+	}
+
+}
