5570d137ac0ebce3c6689573fb39c7356cb6cb18
John Westfield <j.westfield@gmx.de>
2023-09-24 20:53:13 +0200

    initial commit

---
+++ GettersAndSetters/src/application/App.java
@@ -0,0 +1,17 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person person1 = new Person();
+		person1.setName("Joe");
+		person1.setAge(92);
+		
+		System.out.println(person1.getName());
+		System.out.println(person1.getAge());
+
+	}
+
+}
---
+++ GettersAndSetters/src/application/Person.java
@@ -0,0 +1,23 @@
+package application;
+
+public class Person {
+	String name;
+	int age;
+	
+	void setName(String text) {
+		this.name = text;
+	}
+	
+	String getName() {
+		return this.name;
+	}
+	
+	void setAge(int number) {
+		this.age = number;
+	}
+	
+	int getAge() {
+		return this.age;
+	}
+
+}
