9b656ef135087ef4513c3eb5fabcf19c1c393ae0
John Westfield <j.westfield@gmx.de>
2023-09-24 14:17:31 +0200

    initial commit

---
+++ Addition/src/application/Integer.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//int cats;
+		//cats = 7;
+		
+		int cats = 7;
+		int dogs = 5;
+		int rats = 3;
+		
+		int total = cats + dogs + rats;
+		System.out.println("Hi Rats!" + rats);
+		System.out.println("Hello Cats!");
+		System.out.println("Numbers:" + total);
+	}
+
+}
