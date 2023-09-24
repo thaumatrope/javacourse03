7e19a4742cc03a37ce50b63d90ee9f2430eae51b
John Westfield <j.westfield@gmx.de>
2023-09-24 20:41:34 +0200

    initial commit

---
+++ ForLoops/src/application/App.java
@@ -0,0 +1,19 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=0; i < 5; i++) {
+			System.out.println("i is: " + i);
+		}
+		
+		/* Infinite -for- Loop
+		 * for(;;){
+		 * 	System.out.pprintln("This is an infinite loop");
+		 * }
+		 */
+	}
+
+}
