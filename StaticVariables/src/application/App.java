2ab408325b4bdd80c2b986965a3e413cd368fb9a
John Westfield <j.westfield@gmx.de>
2023-09-24 20:42:27 +0200

    initial commit

---
+++ StaticVariables/src/application/App.java
@@ -0,0 +1,19 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cat cat1 = new Cat("Horace");
+		Cat cat2 = new Cat("Mathilda");
+		
+		System.out.println(cat1);
+		System.out.println(cat2);
+		
+		System.out.println(Cat.FOOD);
+		
+		System.out.println(Math.PI);
+	}
+
+}
