8f36f7a2bbdec5c8add4a9ad3e1d3af93cc24927
John Westfield <j.westfield@gmx.de>
2023-09-24 20:53:28 +0200

    initial commit

---
+++ Formatting/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello.");
+		System.out.println("How are you?");
+
+		System.out.println("I am a computer.");
+
+	}
+
+}
