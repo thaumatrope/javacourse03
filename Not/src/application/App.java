029ed1a99b8542d9eb6f622515de34735d2db04f
John Westfield <j.westfield@gmx.de>
2023-09-24 20:48:09 +0200

    initial commit

---
+++ Not/src/application/App.java
@@ -0,0 +1,21 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//unary operator like == and +
+		
+		System.out.println(!(4==5));
+		
+		int value1 = 4;
+		int value2 = 3;
+		
+		if(value1!=value2) {
+			
+			System.out.println("Values are not equal!");
+		}
+	}
+
+}
