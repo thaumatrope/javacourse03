be0ffc1c30fcf03162e2a0d909e2db26cc65f56d
John Westfield <j.westfield@gmx.de>
2023-09-24 20:21:49 +0200

    initial commit

---
+++ VariablesInteger/src/application/Integer.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int cats;
+		cats = 7;
+		
+		System.out.println("Hello Cats!");
+		System.out.println("Numbers:" + cats);
+	}
+
+}
