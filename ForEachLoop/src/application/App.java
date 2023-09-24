9d6248777f868920a109d509b4640a4f13997bba
John Westfield <j.westfield@gmx.de>
2023-09-24 20:41:12 +0200

    initial commit

---
+++ ForEachLoop/src/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] fruits = {"banana", "pear", "apple", "grape"};
+		
+		for(String fruit: fruits) {
+			System.out.println(fruit);
+		}
+
+	}
+
+}
