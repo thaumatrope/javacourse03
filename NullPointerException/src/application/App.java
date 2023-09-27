3c4183379b6d41ace838d65a6c6425ed4ccbbec1
John Westfield <j.westfield@gmx.de>
2023-09-27 05:05:29 +0200

    initial commit

---
+++ NullPointerException/src/application/App.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int value = 5;
+		
+		//runtime exception
+		//System.out.println(value / 0);
+		
+		Scanner scanner = null;
+		
+		scanner.nextLine();
+		
+		
+	}
+
+}
