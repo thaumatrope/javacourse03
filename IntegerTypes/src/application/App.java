30b68303c46bb2bc09b6a05f5c4f128755090a28
John Westfield <j.westfield@gmx.de>
2023-09-28 11:15:07 +0200

    initial commit

---
+++ IntegerTypes/src/application/App.java
@@ -0,0 +1,20 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// byte, short, integer, long
+		byte byteValue = 127;
+		byte byteValuelow = -128;
+		
+		short shortValue = 32767;
+		short shortValueMin = -32768;
+		
+		int intValue = 222200000;
+		long longValue = 12423345345356L;
+
+	}
+
+}
