f8437fc97283e374ba256200b1906f78ae0314c2
John Westfield <j.westfield@gmx.de>
2023-09-29 22:42:19 +0200

    initial commit

---
+++ HandlingKeyPresses/src/guI/MainFrame.java
@@ -0,0 +1,53 @@
+package guI;
+
+import java.awt.BorderLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+
+	private GamePanel gamePanel = new GamePanel();
+	
+	private static final long serialVersionUID = 1L;
+	
+	public MainFrame() {
+		super("Game of Life");
+		
+		
+		setLayout(new BorderLayout());
+		add(gamePanel, BorderLayout.CENTER);
+		
+		addKeyListener(new KeyAdapter() {
+			
+			public void keyPressed(KeyEvent e) {
+//				System.out.print("Key Pressed: " + e.getKeyChar());
+//				System.out.println(" - Key Code: " + e.getKeyCode());
+				
+				int code = e.getKeyCode();
+				switch(code) {
+				case 8: // backspace clear
+					System.out.println("Backspace pressed.");
+					break;
+				case 32: // space next
+					System.out.println("Spacebar pressed.");
+					break;
+				case 10: // enter randomize
+					System.out.println("Enter pressed.");
+					break;
+				default:
+					System.out.println("Some key pressed.");
+				}
+			}
+		});
+		
+		setSize(800, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+
+	
+
+}
