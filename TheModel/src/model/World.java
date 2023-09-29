d4cad6b02d98cc363c47af3c9de74d8b7bbc9b2c
John Westfield <j.westfield@gmx.de>
2023-09-29 13:13:31 +0200

    initial commit

---
+++ TheModel/src/model/World.java
@@ -0,0 +1,24 @@
+package model;
+
+public class World {
+	
+	private int rows;
+	private int columns;
+	
+	private boolean[][] grid;
+	
+	public World(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		
+		grid = new boolean[rows][columns];
+	}
+
+	public boolean getCells(int row, int col) {
+		return grid[row][col];
+	}
+	
+	public void setCells(int row, int col, boolean status){
+		this.grid[row][col] = status;
+	}
+}
