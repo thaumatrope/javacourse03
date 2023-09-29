a5c31475d51023a5c1e792f2f4a5086e539a891b
John Westfield <j.westfield@gmx.de>
2023-09-29 10:13:48 +0200

    initial comment

---
+++ FillingCells/src/application/MainFrame.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+
+	private GamePanel gamePanel = new GamePanel();
+	
+	private static final long serialVersionUID = 1L;
+	
+	public MainFrame() {
+		super("Game of Life");
+		
+		
+		setLayout(new BorderLayout());
+		add(gamePanel, BorderLayout.CENTER);
+		
+		setSize(800, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+
+	
+
+}
