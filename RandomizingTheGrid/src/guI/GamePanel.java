52b29f20ca37beab52d5b702323d7eb642690f7e
John Westfield <j.westfield@gmx.de>
2023-09-30 10:21:56 +0200

    second commit

--- RandomizingTheGrid/src/guI/GamePanel.java
+++ RandomizingTheGrid/src/guI/GamePanel.java
@@ -133,6 +133,7 @@ public class GamePanel extends JPanel {
 		repaint();
 
 	}
+	
 
 }
 
--- RandomizingTheGrid/src/model/World.java
+++ RandomizingTheGrid/src/model/World.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class World {
-	
+
 	private int rows;
 	private int columns;
-	
+
 	private boolean[][] grid;
-	
+
 	public World(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
-		
+
 		grid = new boolean[1024][1024];
 	}
 
 	public boolean getCell(int row, int col) {
 		return grid[row][col];
 	}
-	
-	public void setCell(int row, int col, boolean status){
+
+	public void setCell(int row, int col, boolean status) {
 		this.grid[row][col] = status;
 	}
 
@@ -29,23 +29,19 @@ public class World {
 		// TODO Auto-generated method stub
 		Random random = new Random();
 
-		for(int i = 0; i < ((rows*columns)/10); i++) {
+		for (int i = 0; i < ((rows * columns) / 10); i++) {
 			grid[random.nextInt(rows)][random.nextInt(columns)] = random.nextBoolean();
 		}
 	}
-	
+
 	public void clearModel() {
-		
-//		for(int i = 0; i < rows; i++) {
-//			for(int j = 0; j < columns; j++) {
-//				this.setCell(i, j, false);
-//			}
-//		}	
+
 		// clear by Array.fill()
-		for(int row = 0; row < rows; row++) {
+		for (int row = 0; row < rows; row++) {
 			Arrays.fill(grid[row], false);
 		}
 	}
+
 	
 //	public void resize(int rows, int columns) {
 //		
