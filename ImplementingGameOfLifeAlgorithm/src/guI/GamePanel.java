47d68a359087efcb1c6b9879aadba34502240903
John Westfield <j.westfield@gmx.de>
2023-09-30 21:04:51 +0200

    initial commit

---
+++ ImplementingGameOfLifeAlgorithm/src/guI/GamePanel.java
@@ -0,0 +1,149 @@
+package guI;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JPanel;
+
+import model.World;
+
+public class GamePanel extends JPanel {
+
+	private final static int CELLSIZE = 20;
+	private final static Color backgroundColor = Color.BLACK;
+	private final static Color foregroundColor = Color.GREEN;
+	private final static Color gridColor = Color.GRAY;
+
+	private int topBottomMargin;
+	private int leftRightMargin;
+
+	private World world;
+	private int rows;
+	private int columns;
+
+	private static final long serialVersionUID = 1L;
+
+	public GamePanel() {
+//		this.setBackground(Color.BLUE);
+		addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				System.out.println("X: " + e.getX() + " - Y: " + e.getY());
+				if ((e.getX() >= leftRightMargin) && (e.getX() <= (getWidth() - leftRightMargin))) {
+					int col = (e.getX() - leftRightMargin) / CELLSIZE;
+					if ((e.getY() >= topBottomMargin) && (e.getY() <= (getHeight() - topBottomMargin))) {
+						int row = (e.getY() - topBottomMargin) / CELLSIZE;
+						world.setCell(row, col, !world.getCell(row, col));
+						System.out.print("row(" + row + "),col(" + col + ") ---");
+						System.out.println("Neighbors: " + world.getNeighbors(row, col));
+						world.calcNeighbors(row, col);
+						repaint();
+					}
+				}
+			}
+
+		});	
+		
+		Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> nextGrid(), 500, 500, TimeUnit.MILLISECONDS);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+//		super.paintComponent(g);
+		Graphics2D g2D = (Graphics2D) g;
+
+		int width = getWidth();
+		int height = getHeight();
+
+		leftRightMargin = ((width % CELLSIZE) + CELLSIZE) / 2;
+		topBottomMargin = ((height % CELLSIZE) + CELLSIZE) / 2;
+
+		columns = (width - (leftRightMargin * 2)) / CELLSIZE;
+		rows = (height - (topBottomMargin * 2)) / CELLSIZE;
+
+		System.out.println("We have " + rows + " rows and " + columns + " columns.");
+		if (world == null) {
+			world = new World(rows, columns);
+		}else {
+			world.resize(rows, columns);
+		}
+
+
+		g2D.setColor(backgroundColor);
+		g2D.fillRect(0, 0, width, height);
+	
+		this.drawGrid(g2D, width, height);
+
+		for (int col = 0; col < columns; col++) {
+
+			for (int row = 0; row < rows; row++) {
+				this.fillCell(g2D, row, col, world.getCell(row, col));
+			}
+		}
+
+	}
+
+	private void fillCell(Graphics2D g2D, int row, int col, boolean status) {
+
+		Color color = status ? foregroundColor : backgroundColor;
+		g2D.setColor(color);
+
+		int x = (col * CELLSIZE) + leftRightMargin;
+		int y = (row * CELLSIZE) + topBottomMargin;
+		g2D.fillRect(x + 1, y + 1, CELLSIZE - 1, CELLSIZE - 1);
+
+	}
+
+	private void drawGrid(Graphics2D g2D, int width, int height) {
+		g2D.setColor(gridColor);
+
+		// g2D.drawLine(0, 0, width-1, height-1);
+
+		for (int x = leftRightMargin; x <= (width - leftRightMargin); x += CELLSIZE) {
+			g2D.drawLine(x, topBottomMargin, x, height - topBottomMargin);
+//			System.out.println("Line x1/y1: " + x + "/" + topBottomMargin);
+//			System.out.println("Line x2/y2: " + x + "/" + (height - topBottomMargin));	
+		}
+
+		for (int y = topBottomMargin; y <= (height - topBottomMargin); y += CELLSIZE) {
+			g2D.drawLine(leftRightMargin, y, width - leftRightMargin, y);
+		}
+
+	}
+	
+	public void randomizeGrid() {
+//		System.out.println("Randomize...");
+//		world.clearModel();
+		world.randomizeModel();
+		repaint();
+
+	}
+	
+	public void clearGrid() {
+//		System.out.println("Clearing...");
+		world.clearModel();
+		repaint();
+
+	}
+	
+	public void nextGrid() {
+//		System.out.println("next...");
+		world.nextModel();
+		repaint();
+
+	}
+
+
+}
+
---
+++ ImplementingGameOfLifeAlgorithm/src/model/World.java
@@ -0,0 +1,160 @@
+package model;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class World {
+
+	private int rows;
+	private int columns;
+
+	private boolean[][] grid;
+
+	public World(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+
+		grid = new boolean[rows][columns];
+	}
+
+	public boolean getCell(int row, int col) {
+//		System.out.println("World.getCell() ..../  access grid [" + row + "][" + col + "]");
+//		System.out.println("World.getCell() ..../  grid length [" + grid.length + "][" + grid[0].length + "]");
+		boolean test = false;
+		try {
+			test = grid[row][col];
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("World.getCell() ..../  access grid [" + row + "][" + col + "]");
+			System.out.println("World.getCell() ..../  grid length [" + grid.length + "][" + grid[0].length + "]");
+		}
+		return grid[row][col];
+	}
+
+	public void setCell(int row, int col, boolean status) {
+		this.grid[row][col] = status;
+	}
+
+	public void randomizeModel() {
+		// TODO Auto-generated method stub
+		Random random = new Random();
+
+		for (int i = 0; i < ((rows * columns) / 10); i++) {
+			grid[random.nextInt(rows)][random.nextInt(columns)] = random.nextBoolean();
+		}
+	}
+	
+	
+	public void clearModel() {
+
+//		for(int i = 0; i < rows; i++) {
+//			for(int j = 0; j < columns; j++) {
+//				this.setCell(i, j, false);
+//			}
+//		}	
+		// clear by Array.fill()
+		for (int row = 0; row < rows; row++) {
+			Arrays.fill(grid[row], false);
+		}
+	}
+
+	public void nextModel() {
+		
+		boolean[][] tempGrid = new boolean[rows][columns];
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < columns; col++) {
+				System.out.print("(" + row + "),(" + col + ") --- ");
+				System.out.println("Neighbors: " + this.getNeighbors(row, col) + " / Active: " + this.calcNeighbors(row, col));
+				
+				if(this.calcNeighbors(row, col) < 2) { 			//if neighboring cell count < 2, deactivate cell
+					tempGrid[row][col] = false;
+				}else if(this.calcNeighbors(row, col) > 3) { 	// if neighboring cell count > 3, deactivate cell
+					tempGrid[row][col] = false;
+				}else if(this.calcNeighbors(row, col) == 3) { 	// if neighboring cell count == 3, activate cell
+					tempGrid[row][col] = true;
+				}else if(this.calcNeighbors(row, col) == 2){											// if neighboring cell count == 2, nothing
+					tempGrid[row][col] = grid[row][col];
+				}
+				
+				
+			}
+		}
+		grid = tempGrid;
+	}
+
+	public int getNeighbors(int row, int col) {
+		
+		int neighbors = 0;
+		if ((row == 0) || (row == rows - 1)) {
+			if ((col == 0) || (col == columns - 1)) {
+				neighbors = 3;
+			} else {
+				neighbors = 5;
+			}
+		} else if ((col == 0) || (col == columns - 1)) {
+			if ((row == 0) || (row == rows - 1)) {
+				neighbors = 3;
+			} else {
+				neighbors = 5;
+			}
+		} else {
+			neighbors = 8;
+		}
+		return neighbors;
+	}
+	
+	public int calcNeighbors(int row, int col) {
+		
+		int neighbors = 0;
+		for(int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+			for(int colOffset = -1; colOffset <= 1; colOffset++) {
+				if((rowOffset == 0)&&(colOffset == 0)){
+					continue;
+				}
+				int gridRow = row + rowOffset;
+				int gridCol = col + colOffset;
+				
+				if((gridRow < 0) || (gridCol < 0)){
+					continue;
+				}else if((gridRow >= rows)||(gridCol >= columns)){
+					continue;
+				}
+				System.out.print(gridRow + "," + gridCol + "  ");
+				if(getCell(gridRow, gridCol)){
+					neighbors++;
+				}
+			}				
+		}
+		
+		System.out.println("-- " + neighbors + " active");
+		return neighbors;
+	}
+
+
+	public void resize(int rows, int columns) {
+		
+		if(!((this.rows == rows)&&(this.columns == columns))){
+			System.out.println("World.resize() exit..../ actual grid is[" + this.rows + "][" + this.columns + "]");
+			boolean [][] gridNew = new boolean[rows][columns]; // new grid		
+		    
+			int newRows = (this.rows >= rows) ? rows : this.rows;
+			int newColumns = (this.columns >= columns) ? columns : this.columns;
+	
+			for(int row = 0; row < newRows; row++) {
+				for(int col = 0; col < newColumns; col++) {
+					gridNew[row][col] = grid[row][col];
+				}				
+			}		
+
+			grid = gridNew;
+			gridNew = null;
+			this.rows = rows;
+			this.columns = columns;
+			System.out.println("World.resize() exit..../ new grid is[" + this.rows + "][" + this.columns + "]");
+		}
+//				for{boolean[]tempGridOuter:grid){
+//					System.arraycopy(gridNew[0], 0, tempGridOuter, 0, 5);
+//				}
+				
+		
+	}
+}
