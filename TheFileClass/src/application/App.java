a2a44ad0f25b4db6565c64dc8600f8845de9ad12
John Westfield <j.westfield@gmx.de>
2023-10-02 15:23:19 +0200

    initial commit

--- TheFileClass/src/application/App.java
+++ TheFileClass/src/application/App.java
@@ -2,23 +2,67 @@ package application;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		File currentDirectory = new File(".");  // current directory
+		File currentDirectory1 = new File(".");  // current directory
 		
-		System.out.println(currentDirectory.getAbsolutePath());
+		String myPath = "C:\\Users\\John Westfield\\git\\javacourse03\\TheFileClass"; // double backslash
+		
+		File currentDirectory2 = new File(myPath);
+		
+		System.out.println(currentDirectory1.getAbsolutePath());
+		System.out.println(currentDirectory2.getAbsolutePath());
 		try {
-			System.out.println(currentDirectory.getCanonicalPath());
+			System.out.println(currentDirectory1.getCanonicalPath());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		for(String file:currentDirectory.list()){
+		for(String file:currentDirectory1.list()){
+			System.out.println(file);
+		}
+		
+		
+		// write file
+		String text = "Hello\nthere";
+		
+		try {
+			Files.write(Paths.get("text.txt"), text.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(String file:currentDirectory1.list()){
+			System.out.println(file);
+		}
+		// read file
+		try {
+			String retrievedText = Files.readString(Paths.get("text.txt"));
+			System.out.println(retrievedText);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		// delete file
+		try {
+			Files.deleteIfExists(Paths.get("text.txt"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(String file:currentDirectory1.list()){
 			System.out.println(file);
 		}
 	}
