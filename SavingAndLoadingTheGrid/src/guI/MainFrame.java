01a50696628d9073b5078da602e10ebabd6bd143
John Westfield <j.westfield@gmx.de>
2023-10-05 09:18:04 +0200

    initial commit

---
+++ SavingAndLoadingTheGrid/src/guI/MainFrame.java
@@ -0,0 +1,117 @@
+package guI;
+
+import java.awt.BorderLayout;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class MainFrame extends JFrame{
+
+	private GamePanel gamePanel = new GamePanel();
+	
+	private static final String defaultFilename = "gameoflive.sav";
+	private static final long serialVersionUID = 1L;
+	
+	public MainFrame() {
+		super("Game of Life");
+		
+		
+		setLayout(new BorderLayout());
+		add(gamePanel, BorderLayout.CENTER);
+		
+		MenuItem openItem = new MenuItem("Open");
+		MenuItem saveItem = new MenuItem("Save");
+		Menu fileMenu = new Menu("File");
+		MenuBar menuBar = new MenuBar();
+		fileMenu.add(openItem);
+		fileMenu.add(saveItem);
+		menuBar.add(fileMenu);
+		setMenuBar(menuBar);
+
+		JFileChooser fileChooser = new JFileChooser();
+		var filter = new FileNameExtensionFilter("Game of Life files", "sav");
+		fileChooser.addChoosableFileFilter(filter);
+		fileChooser.setFileFilter(filter);
+			
+
+		openItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fileChooser.setSelectedFile(new File(defaultFilename));
+
+				int userOption = fileChooser.showOpenDialog(MainFrame.this);
+
+				if (userOption == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = fileChooser.getSelectedFile();
+					gamePanel.loadGrid(selectedFile);
+					
+					System.out.println("File loaded from: " + selectedFile.getName());
+				} else {
+					System.out.println("Open command canceled");
+				}
+
+			}
+		});		
+		
+		saveItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fileChooser.setSelectedFile(new File(defaultFilename));
+
+				int userOption = fileChooser.showSaveDialog(MainFrame.this);
+
+				if (userOption == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = fileChooser.getSelectedFile();
+					gamePanel.saveGrid(selectedFile);
+					
+					System.out.println("File Saved as: " + selectedFile.getName());
+				} else {
+					System.out.println("Save command canceled");
+				}
+
+			}
+
+		});
+		
+		addKeyListener(new KeyAdapter() {
+			
+			public void keyPressed(KeyEvent e) {
+//				System.out.print("Key Pressed: " + e.getKeyChar());
+//				System.out.println(" - Key Code: " + e.getKeyCode());
+				
+				int code = e.getKeyCode();
+				switch(code) {
+				case 8: // backspace clear
+					System.out.println("Backspace pressed.");
+					gamePanel.clearGrid();
+					break;
+				case 32: // space next
+					System.out.println("Spacebar pressed.");
+					gamePanel.nextGrid();
+					break;
+				case 10: // enter randomize
+					System.out.println("Enter pressed.");
+					gamePanel.randomizeGrid();
+					break;
+				default:
+					System.out.println("Some key pressed.");
+				}
+			}
+		});
+		
+		setSize(800, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+
+	
+
+}
