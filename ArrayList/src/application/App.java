6ef56a27dd9dff4348f5c2c012c67e2b99f194df
John Westfield <j.westfield@gmx.de>
2023-10-07 08:56:14 +0200

    initial commit

---
+++ ArrayList/src/application/App.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.ArrayList;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(7);
+		list.add(78);
+		list.add(166);
+		
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+		System.out.println(list.get(2));
+	}
+
+}
