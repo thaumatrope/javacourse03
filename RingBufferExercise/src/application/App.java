2cfff4e2674486e6c9c5ed09cef5f7e6286552af
John Westfield <j.westfield@gmx.de>
2023-10-16 16:48:56 +0200

    initial commit

---
+++ RingBufferExercise/src/application/App.java
@@ -0,0 +1,93 @@
+package application;
+
+class RingBuffer {
+
+	private Object[] buffer;
+
+	private int position = 0;
+
+	public RingBuffer(int size) {
+		buffer = new Object[size];
+
+		System.out.println("RingBuffer created with length: " + buffer.length);
+
+	}
+
+	public void add(Object obj) {
+		
+		if(position >= buffer.length)
+			position = 0;
+		
+		buffer[position] = obj;
+		position += 1;	
+		
+	}
+
+	public Object get(int index) {
+
+		if ((index < buffer.length) && (index >= 0)) {
+			return buffer[index];
+		} else {
+
+			System.out.println("Index not within range!");
+			return null;
+		}
+
+	}
+
+	public int size() {
+		int size = 0;
+		for(var item: buffer) {
+			if(item != null) {
+				size++;
+			}
+		}
+
+		return size;
+	}
+
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+
+		for (var element : buffer) {
+			sb.append(element + " / ");
+		}
+
+		return sb.toString();
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RingBuffer buff = new RingBuffer(4);
+
+		System.out.println("RingBuffer size is: " + buff.size());
+		buff.add(7);
+		System.out.println(buff);
+		System.out.println("RingBuffer size is: " + buff.size());
+		buff.add(9);
+		System.out.println(buff);
+		System.out.println("RingBuffer size is: " + buff.size());
+		buff.add(1);
+		System.out.println(buff);
+		System.out.println("RingBuffer size is: " + buff.size());
+		buff.add(22);
+		System.out.println(buff);
+		buff.add(33);
+		System.out.println(buff);
+		System.out.println("RingBuffer size is: " + buff.size());
+		buff.add(44);
+		System.out.println(buff);
+		System.out.println("RingBuffer size is: " + buff.size());
+
+		buff.add(-22);
+		System.out.println(buff);
+		System.out.println("RingBuffer size is: " + buff.size());
+
+	}
+
+}
