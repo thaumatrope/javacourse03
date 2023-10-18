94098b0a3e4e693426eec830eadf34bc5fc5ff4b
John Westfield <j.westfield@gmx.de>
2023-10-18 11:55:12 +0200

    initial commit

---
+++ SetAndSortedSet/src/application/App.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class App {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Set interface
+		Set<Integer> numbers1 = new HashSet<>();
+		Set<Integer> numbers2 = new LinkedHashSet<>();
+		Set<Integer> numbers3 = new TreeSet<>();
+		
+		// Sorted Set interface
+		SortedSet<Integer> numbers4 = new TreeSet<>();
+		
+	}
+
+}
