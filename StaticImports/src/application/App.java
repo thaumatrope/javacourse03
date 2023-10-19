0104d526dfc1a33a0bd2b428d6d9afa8a6f45bff
John Westfield <j.westfield@gmx.de>
2023-10-19 12:18:41 +0200

    initial commit

---
+++ StaticImports/src/application/App.java
@@ -0,0 +1,20 @@
+package application;
+
+import static java.lang.Math.PI; //static constant
+import static java.lang.Math.abs; // static method
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.PI);
+		System.out.println(PI);
+		
+		System.out.println(Math.abs(-12));
+		System.out.println(abs(-12));
+		
+		
+
+	}
+
+}
