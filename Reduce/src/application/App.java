a977df6c3363ee583e9c591609b7615f6bda0df1
John Westfield <j.westfield@gmx.de>
2023-10-31 12:02:26 +0100

    second commit

--- Reduce/src/application/App.java
+++ Reduce/src/application/App.java
@@ -7,10 +7,11 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// IntStream has two versions of reduce
 		System.out.println(IntStream.range(0, 10).count());
 		System.out.println(IntStream.range(0, 10).sum());
 		
-		var value1 = IntStream.range(0, 10).reduce(0, (subtotal, item)->subtotal + item); // starts at 0
+		var value1 = IntStream.range(0, 10).reduce(2, (subtotal, item)->subtotal + item); // starts at 2
 		System.out.println(value1);
 		
 		var value2 = IntStream.range(0, 10).reduce((subtotal, item)->subtotal + item); // starts at first element
